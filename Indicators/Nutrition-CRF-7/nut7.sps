d153f5c4af4eb6ea359a0c8c7a25ee308749de93
olanderb <billyolander@gmail.com>
2022-11-15 11:29:43 -0600

    fixing sample nut  data and syntax

--- Indicators/Nutrition-CRF-7/nut7.sps
+++ Indicators/Nutrition-CRF-7/nut7.sps
@@ -1,15 +1,15 @@
 ﻿* Encoding: UTF-8.
 
-*can only download repeat data as zip file from moda with group names - will update this code to remove group names
+*can only download repeat csv data as zip file from moda with group names - will update this code to remove group names
 *rename to remove group names - because of the variable length SPSS changes name slightly
 
-RENAME VARIABLES (Nutrition_moduleNutProg_submoduleRepeatNutProgNutProgPartic_y = NutProgPartic_yn).
+RENAME VARIABLES (Nutrition_moduleNutProg_submoduleRepeatNutProgPNutProgPartic = PNutProgPartic_yn).
 
 * define variable and value labels
 
-Variable labels NutProgPartic_yn ‘Is ${NutProgParticName} enrolled in the ((insert name/description  of program, to be adapted locally)) programme?’.
+Variable labels PNutProgPartic_yn ‘Is participant enrolled in the ((insert name/description  of program, to be adapted locally)) programme?’.
 
-Value labels NutProgPartic_yn 1 'Yes' 0  'No '.
+Value labels PNutProgPartic_ynNutProgPartic_yn 1 'Yes' 0  'No '.
 
-freq NutProgPartic_yn.
+freq PNutProgPartic_yn.
 
--- Indicators/Nutrition-CRF-7/nut7_tidyverse.R
+++ Indicators/Nutrition-CRF-7/nut7_tidyverse.R
@@ -5,13 +5,14 @@ library(expss)
 #add sample data
 data <- read_csv("~/GitHub/RAMResourcesScripts/Static/Nut_CRF_7_Sample_Survey/Nutrition_module_NutProg_submodule_RepeatNutProg.csv")
 
-#can only download repeat data as zip file from moda with group names - will update this code to remove group names
-data <- data %>% rename(NutProgPartic_yn = 'Nutrition_module/NutProg_submodule/RepeatNutProg/NutProgPartic_yn')
+#can only download repeat csv data as zip file from moda with group names - will update this code to remove group names
+#rename to remove group names
+data <- data %>% rename(PNutProgPartic_yn = 'Nutrition_module/NutProg_submodule/RepeatNutProg/PNutProgPartic_yn')
 
 
 #assign variable and value labels
-var_label(data$NutProgPartic_yn) <- "Is ${NutProgParticName} enrolled in the ((insert name/description  of program, to be adapted locally)) programme?"
-val_lab(data$NutProgPartic_yn) = num_lab("
+var_label(data$PNutProgPartic_yn) <- "Is participant enrolled in the ((insert name/description  of program, to be adapted locally)) programme?"
+val_lab(data$PNutProgPartic_yn) = num_lab("
              0 No
              1 Yes
 ")
@@ -23,11 +24,11 @@ val_lab(data$NutProgPartic_yn) = num_lab("
 
 
 NutProgPartic_yn_table_wide <- data %>% 
-  drop_na(NutProgPartic_yn) %>%
-  count(NutProgPartic_yn_lab = as.character(NutProgPartic_yn)) %>% # if weights are needed use instead the row below 
-  #count(NutProgPartic_yn_lab = as.character(NutProgPartic_yn), wt = nameofweightvariable)
+  drop_na(PNutProgPartic_yn) %>%
+  count(PNutProgPartic_yn_lab = as.character(PNutProgPartic_yn)) %>% # if weights are needed use instead the row below 
+  #count(PNutProgPartic_yn_lab = as.character(PNutProgPartic_yn), wt = nameofweightvariable)
   mutate(Percentage = 100 * n / sum(n)) %>%
   ungroup() %>% select(-n) %>%
-  pivot_wider(names_from = NutProgPartic_yn_lab,
+  pivot_wider(names_from = PNutProgPartic_yn_lab,
               values_from = Percentage,
               values_fill =  0) 
