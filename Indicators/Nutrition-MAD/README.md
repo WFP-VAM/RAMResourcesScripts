0a992ff5d581f9f80918b856f9537935f11c0e3d
olanderb <billyolander@gmail.com>
2023-01-24 07:28:08 -0600

    fixed small labels issues

--- Indicators/Nutrition-MAD/README.md
+++
@@ -1,5 +0,0 @@
-*10 Proportion of children 6-23 months of age who receive a minimum acceptable diet (MAD)*
-
-[Sample Data](https://github.com/WFP-VAM/RAMResourcesScripts/blob/dev/Static/Nut_MAD/MAD_submodule_RepeatMAD.csv)
-
-[Indicator Description on VAM Resource Center](https://www.tutorialsandyou.com/markdown/ 'Learn Markdown')
--- Indicators/Nutrition-MAD/nutMAD_tidyverse.R
+++ Indicators/Nutrition-MAD/nutMAD_tidyverse.R
@@ -3,7 +3,7 @@ library(labelled)
 library(expss)
 
 #add sample data
-data <- read_csv("./RAMResourcesScripts/Static/Nut_MAD_Sample_Survey/MAD_submodule_RepeatMAD.csv")
+data <- read_csv("~/GitHub/RAMResourcesScripts/Static/Nut_MAD_Sample_Survey/MAD_submodule_RepeatMAD.csv")
 
 #can only download repeat csv data as zip file from moda with group names - will update this code with more elegant solution to remove group names or if you download as SPSS you can skip this step
 #rename to remove group names
@@ -43,8 +43,8 @@ var_label(data$PCMADMilk) <- 'Milk from animals, such as fresh, tinned or powder
 var_label(data$PCMADMilkNum) <- 'How many times did ${PCMADChildName} drink milk from animals, such as fresh, tinned or powdered milk?'
 var_label(data$PCMADYogurtDrink) <- 'Yogurt drinks such as [insert local names of common types of yogurt drinks]?'
 var_label(data$PCMADYogurtDrinkNum) <- 'How many times did  ${PCMADChildName} drink Yogurt drinks such as [insert local names of common types of yogurt drinks]?'
-var_label(data$PCMADYogurt) <- 'Yogurt, other than yogurt drinks [insert other commonly consumed grains, including foods made from grains like rice dishes, noodle dishes, etc.]?'
-var_label(data$PCMADStapCer) <- 'Porridge, bread, rice, noodles, pasta or [any additions to this list should meet “Criteria for defining foods and liquids as ‘sources’ of vitamin A”]?'
+var_label(data$PCMADYogurt) <- 'Yogurt, other than yogurt drinks?'
+var_label(data$PCMADStapCer) <- 'Porridge, bread, rice, noodles, pasta or [insert other commonly consumed grains, including foods made from grains like rice dishes, noodle dishes, etc.]?'
 var_label(data$PCMADVegOrg) <- 'Pumpkin, carrots, sweet red peppers, squash or sweet potatoes that are yellow or orange inside? [any additions to this list should meet “Criteria for defining foods and liquids as ‘sources’ of vitamin A”]'
 var_label(data$PCMADStapRoo) <- 'Plantains, white potatoes, white yams, manioc, cassava or [insert other commonly consumed starchy tubers or starchy tuberous roots that are white or pale inside]'
 var_label(data$PCMADVegGre) <- 'Dark green leafy vegetables, such as [insert  commonly consumed vitamin A-rich dark green leafy vegetables]?'
@@ -62,9 +62,8 @@ var_label(data$PCMADSnf) <- 'Specialized Nutritious Foods (SNF) such as [insert 
 var_label(data$PCMADMeals) <- 'How many times did ${PCMADChildName}  eat any solid, semi -solid or soft foods yesterday during the day or night?'
 
 data <- data %>%
-  mutate(across(c(PCMADBreastfeed,PCMADInfFormula,PCMADInfFormulaNum,PCMADMilk,PCMADMilkNum,PCMADYogurtDrink,PCMADYogurtDrinkNum,PCMADYogurt,PCMADStapCer,PCMADVegOrg,PCMADStapRoo,PCMADVegGre,PCMADVegOth,PCMADFruitOrg,PCMADFruitOth,PCMADPrMeatO,PCMADPrMeatPro,PCMADPrMeatF,PCMADPrEgg,PCMADPrFish,PCMADPulse,PCMADCheese,PCMADSnf,PCMADMeals
-                  
-  ), ~labelled(., labels = c(
+  mutate(across(c(PCMADBreastfeed,PCMADInfFormula,PCMADMilk,PCMADYogurtDrink, PCMADYogurt,PCMADStapCer,PCMADVegOrg,PCMADStapRoo,PCMADVegGre,PCMADVegOth,PCMADFruitOrg,
+                  PCMADFruitOth,PCMADPrMeatO,PCMADPrMeatPro,PCMADPrMeatF,PCMADPrEgg,PCMADPrFish,PCMADPulse,PCMADCheese,PCMADSnf), ~labelled(., labels = c(
     "No" = 0,
     "Yes" = 1,
     "Don't know" = 888
@@ -100,10 +99,10 @@ data <- data %>% mutate(MDD_score = MAD_BreastMilk +MAD_PWMDDWStapCer +MAD_Pulse
 data <- data %>% mutate(MDD = case_when(
                               MDD_score >= 5 ~ 1, TRUE ~ 0))
 
-var_label(data$MDD) <- 'Minimum Dietary Diversity'
+var_label(data$MDD) <- 'Minimum Dietary Diversity (MDD)'
 val_lab(data$MDD) = num_lab("
-             0 Does not meet Minimum Dietary Diversity
-             1 Meets Minimum Dietary Diversity
+             0 Does not meet MDD
+             1 Meets MDD
 ")
 
 ##this version for WFP programme monitoring - SNF is counted in meats group
@@ -131,10 +130,10 @@ data <- data %>% mutate(MDD_score_wfp = MAD_BreastMilk_wfp +MAD_PWMDDWStapCer_wf
 data <- data %>% mutate(MDD_wfp = case_when(
   MDD_score_wfp >= 5 ~ 1, TRUE ~ 0))
 
-var_label(data$MDD_wfp) <- 'Minimum Dietary Diversity for WFP program monitoring'
+var_label(data$MDD_wfp) <- 'Minimum Dietary Diversity for WFP program monitoring (MDD)'
 val_lab(data$MDD_wfp) = num_lab("
-             0 Does not meet Minimum Dietary Diversity
-             1 Meets Minimum Dietary Diversity
+             0 Does not meet MDD
+             1 Meets MDD 
 ")
 
 
@@ -143,167 +142,82 @@ table(data$MDD_wfp)
 
 #Calculate Minimum Meal Frequency 6-23 months (MMF) 
 
+#Recode into new variables turning dont know and missing valules into 0  - this makes syntax for calculation simpler
+
+data <- data %>% mutate(PCMADBreastfeed_yn = case_when(
+                                            PCMADBreastfeed == 1 ~ 1, TRUE ~ 0),
+                        PCMADMeals_r = case_when(
+                                            between(PCMADMeals,1,7) == 1 ~ PCMADMeals, TRUE ~ 0),
+                        PCMADInfFormulaNum_r = case_when(
+                                            between(PCMADInfFormulaNum,1,7) == 1 ~ PCMADInfFormulaNum, TRUE ~ 0),
+                        PCMADMilkNum_r = case_when(
+                                            between(PCMADMilkNum,1,7) == 1 ~ PCMADMilkNum, TRUE ~ 0),
+                        PCMADYogurtDrinkNum_r = case_when(
+                                            between(PCMADYogurtDrinkNum,1,7) == 1 ~ PCMADYogurtDrinkNum, TRUE ~ 0)
+                        )
+
 data <- data %>% mutate(MMF = case_when(
-   PCMADBreastfeed == 1 & between(PCMADChildAge_months,6,8) & (PCMADMeals >= 2 & PCMADMeals != 888)  ~ 1, #for child breast fed yesterday - between 6 - 8 months minimum number of meals is 2
-   PCMADBreastfeed == 1 & between(PCMADChildAge_months,9,23) & (PCMADMeals >= 3 & PCMADMeals != 888) ~ 1, #for child breast fed yesterday - for children above 8 months minimum number of meals is 3
+   PCMADBreastfeed_yn == 1 & between(PCMADChildAge_months,6,8) & (PCMADMeals_r >= 2)  ~ 1, #for child breast fed yesterday - between 6 - 8 months minimum number of meals is 2
+   PCMADBreastfeed_yn == 1 & between(PCMADChildAge_months,9,23) & (PCMADMeals_r >= 3) ~ 1, #for child breast fed yesterday - for children above 8 months minimum number of meals is 3
    #for child non breast fed regardless of age , child must have had 1 or more meal of solid/semisolid/soft food and child had 4 or more liquid + solid/semisolid/soft meals 
-   PCMADBreastfeed == 0 & (PCMADMeals >= 1 & PCMADMeals != 888) & (PCMADMeals + PCMADInfFormulaNum + PCMADMilkNum + PCMADYogurtDrinkNum >= 4) ~ 1,
+   PCMADBreastfeed_yn == 0 & (PCMADMeals_r >= 1) & (PCMADMeals_r + PCMADInfFormulaNum_r + PCMADMilkNum_r + PCMADYogurtDrinkNum_r >= 4) ~ 1,
    TRUE ~ 0)
 )
 
-var_label(data$MMF) <- 'Minimum Meal Frequency'
+var_label(data$MMF) <- 'Minimum Meal Frequency (MMF)'
 val_lab(data$MMF) = num_lab("
-             0 Does not meet Minimum Meal Frequency
-             1 Meets Minimum Meal Frequency
+             0 Does not meet MMF
+             1 Meets MMF
 ")
 
-#Minimum Milk Feeding Frequency for non-breastfed children 6-23 months (MMFF) 
+#Calculate Minimum Milk Feeding Frequency for non-breastfed children 6-23 months (MMFF) 
 
-data <- data %>% mutate(PCMADInfFormulaNum_yn = recode())
-
-
-PCMADInfFormulaNum + PCMADMilkNum + PCMADYogurtDrinkNum >= 2 
-
-
-
-#create new variable where missing and dont_knows are considered no - makes the syntax a lot easier
-data <- data %>% mutate(MMF = case_when(
-                              (PCMADBreastfeed != 1 | is.na(PCMADBreastfeed)) ~ 1,
-                              
-                              #& (PCMADInfFormulaNum + PCMADMilkNum + PCMADYogurtDrinkNum >= 2) & (PCMADInfFormulaNum != 888 & PCMADMilkNum != 888 & PCMADYogurtDrinkNum != 888) ~ 1,
+data <- data %>% mutate(MMFF = case_when(
+                              PCMADBreastfeed_yn == 0 & (PCMADInfFormulaNum_r + PCMADMilkNum_r + PCMADYogurtDrinkNum_r >= 2) ~ 1,
                               TRUE ~ 0)
 )
                     
+var_label(data$MMFF) <- 'Minimum Milk Feeding Frequency for non-breastfed children (MMFF)'
+val_lab(data$MMFF) = num_lab("
+             0 Does not meet MMFF
+             1 Meets MMFF
+")
 
 
+# Minimum Acceptable Diet (MAD)
+
+#For breastfed infants: if MDD and MMF are both achieved, then MAD is achieved
+#For nonbreastfed infants: if MDD, MMF and MMFF are all achieved, then MAD is achieved
 
 
+#using MDD for population assesments
+data <- data %>% mutate(MAD = case_when(
+                        PCMADBreastfeed_yn == 1 & MDD == 1 & MMF == 1 ~ 1,
+                        PCMADBreastfeed_yn == 0 & MDD == 1 & MMF == 1 & MMFF == 1 ~ 1,
+                        TRUE ~ 0))
+
+#using MDD for WFP program monitoring
+data <- data %>% mutate(MAD = case_when(
+  PCMADBreastfeed_yn == 1 & MDD_wfp == 1 & MMF == 1 ~ 1,
+  PCMADBreastfeed_yn == 0 & MDD_wfp == 1 & MMF == 1 & MMFF == 1 ~ 1,
+  TRUE ~ 0))
+
+var_label(data$MAD) <- 'Minimum Acceptable Diet (MAD)'
+val_lab(data$MAD) = num_lab("
+             0 Does not meet MAD
+             1 Meets MAD
+")
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#Calculate 2 MDDW indicators based on WFP guidelines https://docs.wfp.org/api/documents/WFP-0000140197/download/ pg.8
-#Standard MDDW Indicator for population based surveys - counts SNF in home group (refer to https://docs.wfp.org/api/documents/WFP-0000139484/download/ for "home group")
-#WFP Modified MDDW WFP programme monitoring - counts SNF in "Meat, poultry and fish" Category
-
-#Standard MDDW method - in this example SNF home group will be grains
-#in this example fortified foods PWMDDWFortFoodwflour,PWMDDWFortFoodmflour,PWMDDWFortFoodrice,PWMDDWFortFooddrink will also count in grains
-#classifying PWMDDWFortFoodother_oth will likely involve classifying line by line 
-data <- data %>% mutate(
-                        MDDW_Staples = case_when(
-                            PWMDDWStapCer == 1 |  PWMDDWStapRoo == 1 | PWMDDWSnf == 1 | PWMDDWFortFoodwflour == 1 | PWMDDWFortFoodmflour == 1 | PWMDDWFortFoodrice == 1 | PWMDDWFortFooddrink == 1 ~ 1, TRUE ~ 0),
-                        MDDW_Pulses = case_when(
-                            PWMDDWPulse == 1 ~ 1, TRUE ~ 0),
-                        MDDW_NutsSeeds  = case_when(
-                            PWMDDWNuts == 1 ~ 1, TRUE ~ 0),
-                        MDDW_Dairy  = case_when(
-                            PWMDDWDairy == 1 | PWMDDWMilk == 1 ~ 1, TRUE ~ 0),
-                        MDDW_MeatFish  = case_when(
-                            PWMDDWPrMeatO == 1 | PWMDDWPrMeatF == 1 | PWMDDWPrMeatPro == 1 | PWMDDWPrMeatWhite == 1 |  PWMDDWPrFish == 1 ~ 1, TRUE ~ 0),
-                        MDDW_Eggs  = case_when(
-                            PWMDDWPrEgg == 1 ~ 1, TRUE ~ 0),
-                        MDDW_LeafGreenVeg  = case_when(
-                            PWMDDWVegGre == 1 ~ 1, TRUE ~ 0),
-                        MDDW_VitA  = case_when(
-                            PWMDDWVegOrg == 1 | PWMDDWFruitOrg == 1 ~ 1, TRUE ~ 0),
-                        MDDW_OtherVeg  = case_when(
-                            PWMDDWVegOth == 1 ~ 1, TRUE ~ 0),
-                        MDDW_OtherFruits  = case_when(
-                            PWMDDWFruitOth == 1 ~ 1, TRUE ~ 0)
-                        )
-
-#WFP MDDW method for program monitoring - SNF will count in the meats group
-#in this example fortified foods PWMDDWFortFoodwflour,PWMDDWFortFoodmflour,PWMDDWFortFoodrice,PWMDDWFortFooddrink will also count in grains
-#classifying PWMDDWFortFoodother_oth will likely involve classifying line by line 
-data <- data %>% mutate(
-  MDDW_Staples_wfp = case_when(
-    PWMDDWStapCer == 1 |  PWMDDWStapRoo == 1 | PWMDDWFortFoodwflour == 1 | PWMDDWFortFoodmflour == 1 | PWMDDWFortFoodrice == 1 | PWMDDWFortFooddrink == 1 ~ 1, TRUE ~ 0),
-  MDDW_Pulses_wfp = case_when(
-    PWMDDWPulse == 1 ~ 1, TRUE ~ 0),
-  MDDW_NutsSeeds_wfp  = case_when(
-    PWMDDWNuts == 1 ~ 1, TRUE ~ 0),
-  MDDW_Dairy_wfp  = case_when(
-    PWMDDWDairy == 1 | PWMDDWMilk == 1 ~ 1, TRUE ~ 0),
-  MDDW_MeatFish_wfp  = case_when(
-    PWMDDWPrMeatO == 1 | PWMDDWPrMeatF == 1 | PWMDDWPrMeatPro == 1 | PWMDDWPrMeatWhite == 1 |  PWMDDWPrFish == 1 | PWMDDWSnf == 1 ~ 1, TRUE ~ 0),
-  MDDW_Eggs_wfp  = case_when(
-    PWMDDWPrEgg == 1 ~ 1, TRUE ~ 0),
-  MDDW_LeafGreenVeg_wfp  = case_when(
-    PWMDDWVegGre == 1 ~ 1, TRUE ~ 0),
-  MDDW_VitA_wfp  = case_when(
-    PWMDDWVegOrg == 1 | PWMDDWFruitOrg == 1 ~ 1, TRUE ~ 0),
-  MDDW_OtherVeg_wfp  = case_when(
-    PWMDDWVegOth == 1 ~ 1, TRUE ~ 0),
-  MDDW_OtherFruits_wfp  = case_when(
-    PWMDDWFruitOth == 1 ~ 1, TRUE ~ 0)
-)
-
-
-#calculate MDDW variable for both methods by adding together food groups and classifying whether the woman consumed 5 or more food groups
-
-#Standard MDDW method where SNF is counted in grains
-data <- data %>% mutate(MDDW = MDDW_Staples +MDDW_Pulses +MDDW_NutsSeeds +MDDW_Dairy +MDDW_MeatFish +MDDW_Eggs +MDDW_LeafGreenVeg +MDDW_VitA +MDDW_OtherVeg +MDDW_OtherFruits)
-#count how many women consumed 5 or more groups
-data <- data %>% mutate(MDDW_5 = case_when(
-                        MDDW >= 5 ~ ">=5", TRUE ~ "<5"
-))
-#WFP MDDW method for program monitoring - SNF will count in the meats group
-data <- data %>% mutate(MDDW_wfp = MDDW_Staples_wfp +MDDW_Pulses_wfp +MDDW_NutsSeeds_wfp +MDDW_Dairy_wfp +MDDW_MeatFish_wfp +MDDW_Eggs_wfp +MDDW_LeafGreenVeg_wfp +MDDW_VitA_wfp +MDDW_OtherVeg_wfp +MDDW_OtherFruits_wfp)
-#count how many women consumed 5 or more groups
-data <- data %>% mutate(MDDW_5_wfp = case_when(
-  MDDW_wfp >= 5 ~ ">=5", TRUE ~ "<5"
-))
-
-#creates a table of the weighted percentage of Standard MDDW Method - MDDW_5 
+#creates a table of the weighted percentage of MAD
 #and providing the option to use weights if needed
 
-MDDW_5_table_wide <- data %>% 
-  drop_na(MDDW_5) %>%
-  count(MDDW_5) %>% # if weights are needed use instead the row below 
-  #count(MDDW_5, wt = nameofweightvariable)
+MAD_table_wide <- data %>% 
+  drop_na(MAD) %>%
+  count(MAD) %>% # if weights are needed use instead the row below 
+  #count(MAD, wt = nameofweightvariable)
   mutate(Percentage = 100 * n / sum(n)) %>%
   ungroup() %>% select(-n) %>%
-  pivot_wider(names_from = MDDW_5,
+  pivot_wider(names_from = MAD,
               values_from = Percentage,
               values_fill =  0) 
-
-#creates a table of the weighted percentage of WFP MDDW method for program monitoring MDDW_5_wfp
-#and providing the option to use weights if needed
-
-
-MDDW_5_wfp_table_wide <- data %>% 
-  drop_na(MDDW_5_wfp) %>%
-  count(MDDW_5_wfp) %>% # if weights are needed use instead the row below 
-  #count(MDDW_5_wfp, wt = nameofweightvariable)
-  mutate(Percentage = 100 * n / sum(n)) %>%
-  ungroup() %>% select(-n) %>%
-  pivot_wider(names_from = MDDW_5_wfp,
-              values_from = Percentage,
-              values_fill =  0) 
-
-
-
-
-
