7359830daa59747464d1d797e936048a60e6c32e
olanderb <billyolander@gmail.com>
2023-02-20 13:58:16 -0600

    add sample data and syntax for head of household version

--- Indicators/Protection-CRF-CC1-4/pro-CC1-4_hoh_tidyverse.R
+++ Indicators/Protection-CRF-CC1-4/pro-CC1-4_hoh_tidyverse.R
@@ -3,43 +3,94 @@ library(labelled)
 library(expss)
 
 #add sample data
-data <- read_csv("Static/PROP_AAP_CRF_Sample_Survey.csv")
+data <- read_csv("~/GitHub/RAMResourcesScripts/Static/PRO_WG_Sample_Survey/HoHMethod/PRO_WG_HoH_Sample_Survey.csv")
 
-#assign variable and value labels
-var_label(data$HHAsstRespect) <- "Do you think WFPandor partner staff have treated you and members of your household respectfully?"
-var_label(data$HHDTPDign) <- "Do you think the conditions of WFP programme sites are dignified?"
+
+#assign variable and value labels 
+var_label(data$HHAsstWFPRecCashYN1Y) <- "Did your household receive cash-based WFP assistance in the last 12 months?"
+var_label(data$HHAsstWFPRecInKindYN1Y) <- "Did your household receive in-kind WFP assistance in the last 12 months?"
+var_label(data$HHAsstWFPRecCapBuildYN1Y) <- "Did you household receive WFP capacity building assistance in the last 12 months?"
+
+var_label(data$HHHSex) <- "What is the sex of the head of the household?"
+var_label(data$HHHAge) <- "Age of the head of the household"
+
+var_label(data$HHHDisabSee) <- "Does the head of household have difficulty seeing, even if wearing glasses? Would you say…"
+var_label(data$HHHDisabHear) <- "Does the head of household have difficulty hearing, even if using a hearing aid(s)? Would you say…"
+var_label(data$HHHDisabWalk) <- "Does the head of household have difficulty walking or climbing steps? Would you say…"
+var_label(data$HHHDisabRemember) <- "Does the head of household have difficulty remembering or concentrating? Would you say…"
+var_label(data$HHHDisabUnderstand) <- "Using his or her usual language, does the head of household hav e difficulty communicating, for example understanding or being understood? Would you say…"
+var_label(data$HHHDisabWash) <- "Does the head of household have difficulty with self-care, such as washing all over or dressing? Would you say…"
+
 
 data <- data %>%
-  mutate(across(c(HHAsstRespect, HHDTPDign), ~labelled(., labels = c(
+  mutate(across(c(HHAsstWFPRecCashYN1Y, HHAsstWFPRecInKindYN1Y, HHAsstWFPRecCapBuildYN1Y), ~labelled(., labels = c(
     "No" = 0,
     "Yes" = 1
-    ))))
+  ))))
 
-#calculate indicator and assign variable label & name
-data <- data %>% mutate(HHAsstRespectDign = case_when(
-  HHAsstRespect == 1 & HHDTPDign == 1 ~ 1,
+val_lab(data$HHHSex) = num_lab("
+             0 Female
+             1 Male
+")
+
+data <- data %>%
+  mutate(across(c(HHHDisabSee,HHHDisabHear,HHHDisabWalk,HHHDisabRemember,HHHDisabUnderstand,HHHDisabWash), ~labelled(., labels = c(
+    "No difficulty" = 1,
+    "Some difficulty" = 2,
+    "A lot of difficulty" = 3,
+    "Cannot do at all" = 4,
+    "Don't know" = 888,
+    "Refuse" = 999
+  ))))  
+
+#calculate whether the respondent had "A lot of difficulty" or "Cannot do at all" for any of the 6 questions.
+data <- data %>% mutate(HHHDisabCat3 = case_when(
+  between(HHHDisabSee,3,4) | between(HHHDisabHear,3,4) | between(HHHDisabWalk,3,4) | between(HHHDisabRemember,3,4) | between(HHHDisabUnderstand,3,4)| between(HHHDisabWash,3,4) ~ 1,
   TRUE ~ 0
 ))
-var_label(data$HHAsstRespectDign) <- "Treated with respect while engaging in WFP programs"
-val_lab(data$HHAsstRespectDign) = num_lab("
-             0 No
-             1 Yes
+
+val_lab(data$HHHDisabCat3) = num_lab("
+             0 without disability (category 3 criteria)
+             1 with disability (category 3 criteria)
 ")
 
 
-#creates a table of the weighted percentage of HHAsstRespectDign by
+#creates a table of the weighted percentage of type of assistance recieve by HHHDisabCat3
 #creating a temporary variable to display value labels 
 #and providing the option to use weights if needed
 
-
-HHAsstRespectDign_table_wide <- data %>% 
-  drop_na(HHAsstRespectDign) %>%
-  count(HHAsstRespectDign_lab = as.character(HHAsstRespectDign)) %>% # if weights are needed use instead the row below 
-  #count(HHAsstRespectDign_lab = as.character(HHAsstRespectDign), wt = nameofweightvariable)
+#cash
+HHAsstWFPRecCashYN1Y_table_wide <- data %>% group_by(HHHDisabCat3_lab = as_factor(HHHDisabCat3)) %>% 
+  drop_na(HHAsstWFPRecCashYN1Y) %>%
+  count(HHAsstWFPRecCashYN1Y_lab = as_factor(HHAsstWFPRecCashYN1Y)) %>% # if weights are needed use instead the row below 
+  #count(HHAsstWFPRecCashYN1Y_lab = as.character(HHAsstWFPRecCashYN1Y), wt = nameofweightvariable)
   mutate(Percentage = 100 * n / sum(n)) %>%
   ungroup() %>% select(-n) %>%
-  pivot_wider(names_from = HHAsstRespectDign_lab,
+  pivot_wider(names_from = HHAsstWFPRecCashYN1Y_lab,
               values_from = Percentage,
               values_fill =  0) 
+
+#inkind
+HHAsstWFPRecInKindYN1Y_table_wide <- data %>% group_by(HHHDisabCat3_lab = as_factor(HHHDisabCat3)) %>% 
+  drop_na(HHAsstWFPRecInKindYN1Y) %>%
+  count(HHAsstWFPRecInKindYN1Y_lab = as_factor(HHAsstWFPRecInKindYN1Y)) %>% # if weights are needed use instead the row below 
+  #count(HHAsstWFPRecInKindYN1Y_lab = as.character(HHAsstWFPRecInKindYN1Y), wt = nameofweightvariable)
+  mutate(Percentage = 100 * n / sum(n)) %>%
+  ungroup() %>% select(-n) %>%
+  pivot_wider(names_from = HHAsstWFPRecInKindYN1Y_lab,
+              values_from = Percentage,
+              values_fill =  0) 
+
+#capacity building
+HHAsstWFPRecCapBuildYN1Y_table_wide <- data %>% group_by(HHHDisabCat3_lab = as_factor(HHHDisabCat3)) %>% 
+  drop_na(HHAsstWFPRecCapBuildYN1Y) %>%
+  count(HHAsstWFPRecCapBuildYN1Y_lab = as_factor(HHAsstWFPRecCapBuildYN1Y)) %>% # if weights are needed use instead the row below 
+  #count(HHAsstWFPRecCapBuildYN1Y_lab = as.character(HHAsstWFPRecCapBuildYN1Y), wt = nameofweightvariable)
+  mutate(Percentage = 100 * n / sum(n)) %>%
+  ungroup() %>% select(-n) %>%
+  pivot_wider(names_from =HHAsstWFPRecCapBuildYN1Y_lab,
+              values_from = Percentage,
+              values_fill =  0) 
+
 
   
