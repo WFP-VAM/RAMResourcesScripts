7385db78fef3fdbd3c44f6bc001716a8dba3e545
Nicole Wu <37033705+nicolefindstar@users.noreply.github.com>
2023-04-25 15:24:55 +0200

    Update FCSN_indicator_tidyverse.R
    Updated labels

--- Indicators/Food-consumption-score-nutrition/FCSN_indicator_tidyverse.R
+++ Indicators/Food-consumption-score-nutrition/FCSN_indicator_tidyverse.R
@@ -7,21 +7,19 @@ data <- read_csv("~/GitHub/RAMResourcesScripts/Static/FCSN_Sample_Survey.csv")
 
 #assign variable and value labels
 #variable labels
-var_label(data$FCSNPrMeatF) <- "Flesh meat, such as:"
-var_label(data$FCSNPrMeatO) <- "Organ meat, such as:"
-var_label(data$FCSNPrFish) <- "Fish/shellfish, such as:"
-var_label(data$FCSNPrEggs) <- "Eggs:"
-var_label(data$FCSNVegOrg) <- "Orange vegetables (vegetables rich in Vitamin A), such as:"
-var_label(data$FCSNVegGre) <- "Green leafy vegetables, such as:"
-var_label(data$FCSNFruiOrg) <- "Orange fruits (Fruits rich in Vitamin A), such as:"
+var_label(data$FCSNPrMeatF) <- "Consumption in past 7 days: Flesh meat"
+var_label(data$FCSNPrMeatO) <- "Consumption in past 7 days: Organ meat"
+var_label(data$FCSNPrFish)  <- "Consumption in past 7 days: Fish/shellfish"
+var_label(data$FCSNPrEggs)  <- "Consumption in past 7 days: Eggs"
+var_label(data$FCSNVegOrg)  <- "Consumption in past 7 days: Orange vegetables (vegetables rich in Vitamin A)"
+var_label(data$FCSNVegGre)  <- "Consumption in past 7 days: Green leafy vegetables"
+var_label(data$FCSNFruiOrg) <- "Consumption in past 7 days: Orange fruits (Fruits rich in Vitamin A)"
 	
 #recode "n/a" values to 0
 vars2recode <- c("FCSNPrMeatF","FCSNPrMeatO","FCSNPrFish","FCSNPrEggs","FCSNVegOrg","FCSNVegGre","FCSNFruiOrg")
 
 data <- data %>% mutate_at(vars2recode, ~replace(., . == "n/a", "0"))
-
 data <- data %>% mutate_at(vars2recode, as.numeric)
-
   
 #compute aggregates of key micronutrient consumption of vitamin, iron and protein 
 data <- data %>% mutate(FGVitA = FCSDairy +FCSNPrMeatO +FCSNPrEggs +FCSNVegOrg +FCSNVegGre +FCSNFruiOrg)
@@ -48,6 +46,6 @@ data <- data %>%
     "Consumed at least 7 times" = 3
   ))))
 
-var_label(data$FGVitACat) <- "Consumption of vitamin A-rich foods"
-var_label(data$FGProteinCat) <- "Consumption of protein-rich foods"
-var_label(data$FGHIronCat) <- "Consumption of hem iron-rich foods"
+var_label(data$FGVitACat) <- "Consumption group of vitamin A-rich foods"
+var_label(data$FGProteinCat) <- "Consumption group of protein-rich foods"
+var_label(data$FGHIronCat) <- "Consumption group of hem iron-rich foods"
