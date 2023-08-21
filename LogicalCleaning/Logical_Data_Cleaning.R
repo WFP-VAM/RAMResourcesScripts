189fd53790896f8693a0f4fb16521a46863b6985
axmedmaxamuud <52702274+axmedmaxamuud@users.noreply.github.com>
2023-08-21 13:16:55 +0300

    logical cleaning

--- LogicalCleaning/Logical_Data_Cleaning.R
+++ LogicalCleaning/Logical_Data_Cleaning.R
@@ -23,3 +23,29 @@ check_form_time <- survey_time(df = raw_data, time_min = 20, time_max = 40) %>%
 # merge check_form_time with your logbook using the rbind function
 my_logbook <- rbind(my_logbook, check_form_time)
 
+# check other responses 
+# first make a list of all other columns included in your data
+other_columns <- c("camp_structure_other")
+
+check_others <- check_other_responses(data = raw_data, other_columns = other_columns)
+
+# include other checks dataframe into your logbook
+my_logbook <- rbind(my_logbook, check_others)
+
+# check the age of respondents
+check_ki_age <- raw_data %>% 
+  filter(ki_age < 18) %>% 
+  log_sheet(question.name = "ki_age",
+            issue = "the respondent is under age, please check if there was gardian",
+            action = "check")
+my_logbook <- rbind(my_logbook, check_ki_age)
+
+# check if there are any duplicated uuids in your data
+check_dup_surveys <- check_duplicate_uuid(data = raw_data) # this function will return the list of duplicated uuids in the console
+
+# check the non-response rate of survey variables
+check_missing_data <- get_na_response_rates(data = raw_data)
+
+## Step 4: Export your cleaning log book and share it with field staff for making actions and filling the new.value column
+
+
