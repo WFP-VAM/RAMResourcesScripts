11d32fbd158c919b21809b4c6c5bee4252d93495
lorenzomoncada <116555633+lorenzomoncada@users.noreply.github.com>
2023-09-22 17:54:17 +0200

    Update ECMEN_Including_Assistance_Monitoring_tidyverse.R

--- Indicators/Economic-Capacity-to-Meet-Essential-Needs/ECMEN_Including_Assistance_Monitoring_tidyverse.R
+++ Indicators/Economic-Capacity-to-Meet-Essential-Needs/ECMEN_Including_Assistance_Monitoring_tidyverse.R
@@ -8,7 +8,7 @@
 ## ECMEN calculation is based on the standard module available here: 
 #   https://docs.wfp.org/api/documents/WFP-0000115416/download/
 ## Detailed guidance on the computation of the ECMEN can be found here: 
-#   add link to standalone guidance when available
+#  https://docs.wfp.org/api/documents/WFP-0000145644/download/
 
 ## Note: 
 #    The computation of the ECMEN requires having already established a Minimum 
