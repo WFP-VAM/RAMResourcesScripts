012fe7d05eff5952e359549b3348c4d8b06f1ef1
axmedmaxamuud <52702274+axmedmaxamuud@users.noreply.github.com>
2023-10-04 09:56:08 +0300

    variable naming aligned with the survey designer

--- LogicalCleaning/Logical_Data_Cleaning.R
+++ LogicalCleaning/Logical_Data_Cleaning.R
@@ -10,13 +10,12 @@ library(openxlsx)
 library(cleanR)
 
 ## Step 2: load data & create blank cleaning log book file
-raw_data <- cleanR::data
+raw_data <- cleanR::survey_data # this is dumpy data generated from SD platform
 my_logbook <- cleanR::logbook() # this will be blank logbook that you can use to export at the end.
 
-## Step 3: Start running your data checking 
+## Step 3: Start running your data checking exercise
 # check survey duration
 check_form_time <- survey_time(df = raw_data, time_min = 20, time_max = 40) %>% 
-  filter(CHECK_interview_duration %in% c("Too long", "Too short")) %>% 
   log_sheet(question.name = "interview_duration", 
             issue = "survey filled with less/long time, please check",
             action = "check")
@@ -25,17 +24,19 @@ my_logbook <- rbind(my_logbook, check_form_time)
 
 # check other responses 
 # first make a list of all other columns included in your data
-other_columns <- c("camp_structure_other")
+other_columns <- c("HHAsstSecurityRisk_oth",
+                   "HHAsstAccessWhat_oth")
 
 check_others <- check_other_responses(data = raw_data, other_columns = other_columns)
 
 # include other checks dataframe into your logbook
 my_logbook <- rbind(my_logbook, check_others)
 
+## You can run project specefic checks by using dplyr functions and than use the log_sheet function to reshape your data into clog format
 # check the age of respondents
 check_ki_age <- raw_data %>% 
-  filter(ki_age < 18) %>% 
-  log_sheet(question.name = "ki_age",
+  filter(RESPAge < 18) %>% 
+  log_sheet(question.name = "RESPAge",
             issue = "the respondent is under age, please check if there was gardian",
             action = "check")
 my_logbook <- rbind(my_logbook, check_ki_age)
