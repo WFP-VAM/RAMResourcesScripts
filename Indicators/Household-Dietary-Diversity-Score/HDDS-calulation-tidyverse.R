d387e4366e336c4b2d4592b8f42d50caf1cda293
nicolefindstar <yue.wu@berkeley.edu>
2024-07-29 20:41:22 +0200

    Minor edits to HDDS
    Drop the duplicated rename part

--- Indicators/Household-Dietary-Diversity-Score/HDDS-calulation-tidyverse.R
+++ Indicators/Household-Dietary-Diversity-Score/HDDS-calulation-tidyverse.R
@@ -5,28 +5,31 @@
 
 # Load Packages --------------------------------------------------------------#
 library(tidyverse)
+library(dplyr)
 library(labelled)
+library(expss)
+library(haven)
 
 # Load Sample Data ------------------------------------------------------------#
-# df <- read_csv("path_to_your_file.csv")
 
-# Rename variables to match the script ----------------------------------------#
-df <- df %>%
-  rename(HDDSStapCer  = 'HDDSStapCer',
-         HDDSStapRoot = 'HDDSStapRoot',
-         HDDSVeg      = 'HDDSVeg',
-         HDDSFruit    = 'HDDSFruit',
-         HDDSPrMeat   = 'HDDSPrMeat',
-         HDDSPrEggs   = 'HDDSPrEggs',
-         HDDSPrFish   = 'HDDSPrFish',
-         HDDSPulse    = 'HDDSPulse',
-         HDDSDairy    = 'HDDSDairy',
-         HDDSFat      = 'HDDSFat',
-         HDDSSugar    = 'HDDSSugar',
-         HDDSCond     = 'HDDSCond')
+# data <- read_csv("path_to_your_file.csv")
+
+# Assign variable labels -------------------------------------------------------#
+var_label(data$HDDSStapCer)  <- "Cereals consumption in the previous 24 hours"
+var_label(data$HDDSStapRoot) <- "Roots and tubers consumption in the previous 24 hours"
+var_label(data$HDDSVeg)      <- "Vegetable consumption in the previous 24 hours"
+var_label(data$HDDSFruit)    <- "Fruit consumption in the previous 24 hours"
+var_label(data$HDDSPrMeat)   <- "Meat/poultry consumption in the previous 24 hours"
+var_label(data$HDDSPrEggs)   <- "Eggs consumption in the previous 24 hours"
+var_label(data$HDDSPrFish)   <- "Fish consumption in the previous 24 hours"
+var_label(data$HDDSPulse)    <- "Pulses/legumes consumption in the previous 24 hours"
+var_label(data$HDDSDairy)    <- "Milk and dairy product consumption in the previous 24 hours"
+var_label(data$HDDSFat)      <- "Oil/fats consumption in the previous 24 hours"
+var_label(data$HDDSSugar)    <- "Sugar/honey consumption in the previous 24 hours"
+var_label(data$HDDSCond)     <- "Miscellaneous/condiments consumption in the previous 24 hours"
 
 # Compute HDDS ---------------------------------------------------------------#
-df <- df %>%
+data <- data %>%
   mutate(HDDS = HDDSStapCer + HDDSStapRoot + HDDSVeg + HDDSFruit +
                 HDDSPrMeat + HDDSPrEggs + HDDSPrFish + HDDSPulse +
                 HDDSDairy + HDDSFat + HDDSSugar + HDDSCond) %>%
@@ -37,13 +40,13 @@ df <- df %>%
   ))
 
 # Assign value labels
-val_lab(df$HDDSCat_IPC) <- num_lab("
+val_lab(data$HDDSCat_IPC) <- num_lab("
   1 '0-2 food groups (phase 4 to 5)'
   2 '3-4 food groups (phase 3)'
   3 '5-12 food groups (phase 1 to 2)'
 ")
 
 # View the result
-print(df %>% select(HDDS, HDDSCat_IPC))
+print(data %>% select(HDDS, HDDSCat_IPC))
 
 # End of Scripts
